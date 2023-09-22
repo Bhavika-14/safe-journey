81ef29b1cd4a3fe8c9873847cfdeec7c6e0c47df
Bhavika Aggarwal <91602025+Bhavika-14@users.noreply.github.com>
2023-09-22 12:14:32 +0530

    completed final component

--- frontend/src/components/result.js
+++ frontend/src/components/result.js
@@ -3,11 +3,11 @@ import React from 'react'
 const Result = ({setResult}) => {
   return (
     <div className='flex justify-center'>
-        <div>
-            <div>Meet at</div>
-            <div></div>
-            <div>Message</div>
-            <div>Cancel Journey</div>
+        <div className='w-[85%]'>
+            <div className='mt-4'>Meet Julia at this place</div>
+            <div className='justify-center flex'><div className='my-4 h-32 w-32 bg-black rounded-full'></div></div>
+            <div className='mt-2 p-2 text-white text-center bg-black rounded-md cursor-pointer'>Message</div>
+            <div className='mt-2 p-2 text-white text-center bg-black rounded-md cursor-pointer'>Cancel Journey</div>
         </div>
     </div>
   )
